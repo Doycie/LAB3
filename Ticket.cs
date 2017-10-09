c5cd077631d713d49b5b7b3002cff96e75891450
Doycie <imcody97@gmail.com>
2017-10-09 16:32:20 +0200

    added ticket

--- Ticket.cs
+++ Ticket.cs
@@ -5,7 +5,41 @@ using System.Text;
 
 namespace Lab3
 {
+    class Trajectory {
+
+        public String Destination { get; set; }
+        public String Origin { get; set; }
+        public int trajectoryValue { get; set; }
+    }
+    class TicketClass {
+        
+        bool firstClass { get; set; }
+    }
+    class TicketDate {
+        private DateTime date { get; set; }
+        public String dateToString()
+        {
+            return date.ToString();
+        }
+    }
+    class TicketType
+    {
+        bool isSingle { get; set; }
+        bool isReturn { get; set; }
+        bool isDayCard { get; set; }
+        bool isBikeCard { get; set; }
+    }
+
+
+
+
+
     class Ticket
     {
+        public Trajectory trajectory;
+        public TicketClass ticketClass;
+        public TicketDate ticketDate;
+        public TicketType ticketType;
+
     }
 }
