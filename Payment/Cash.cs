4dcac50ca577a23009b602d6216f16933f77f61b
Rachel <Rachel@RACHEL-PC>
2017-10-10 12:04:57 +0200

    Payment done

---
+++ Payment/Cash.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3
+{
+    public class IKEAMyntAtare2000
+    {
+        public void starta()
+        {
+            MessageBox.Show("Välkommen till IKEA Mynt Ätare 2000");
+        }
+
+        public void stoppa()
+        {
+            MessageBox.Show("Hejdå!");
+        }
+
+        public void betala(int pris)
+        {
+            MessageBox.Show(pris + " cent");
+        }
+    }
+
+    class Cash : Payment
+    {
+        IKEAMyntAtare2000 coinMachine;
+
+        public Cash(float price)
+        : base(price)
+        {
+            coinMachine = new IKEAMyntAtare2000();
+        }
+
+        protected override void Pay()
+        {
+            coinMachine.starta();
+            coinMachine.betala((int)Math.Round(Price * 100));
+            coinMachine.stoppa();
+            Success = true;
+        }
+    }
+}
---
+++ Payment/Payment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3
+{
+    abstract class Payment
+    {
+        protected float Price;
+        protected bool Success;
+
+        public Payment(float price)
+        {
+            Price = price;
+            Success = false;
+        }
+
+        protected abstract void Pay();
+
+
+        public bool isSuccessfull()
+        {
+            return Success;
+        }
+    }
+}
+
