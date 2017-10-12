008d88f13d743cf34ec4bee7cbe0cea886d8e7b0
Broek, M.G.M. van den (Mark) <m.g.m.vandenbroek@students.uu.nl>
2017-10-12 11:43:00 +0200

    Ticket changes

--- Ticket.cs
+++ Ticket.cs
@@ -11,48 +11,19 @@ namespace Lab3
         public String Origin { get; set; }
         public int trajectoryValue { get; set; }
     }
-    public class TicketClass {
-        
-        public bool firstClass { get; set; }
-    }
-    public class TicketDate {
-        private DateTime date { get; set; }
-        public String dateToString()
-        {
-            return date.ToString();
-        }
-    }
-    public  class TicketType
-    {
-        public bool isSingle { get; set; }
-        public bool isReturn { get; set; }
-        public bool isDayCard { get; set; }
-        public bool isBikeCard { get; set; }
-    }
-    public class TicketDiscount
-    {
-        public int discount { get; set; }
-    }
-
-
-
-
 
     public class Ticket
     {
         public Trajectory trajectory;
+        public enum TicketClass { First, Second };
         public TicketClass ticketClass;
-        public TicketDate ticketDate;
-        public TicketType ticketType;
-        public TicketDiscount ticketDiscount;
+        public bool isReturn;
+        public enum Discount { Zero, Twenty, Forty };
+        public Discount discount;
 
         public Ticket()
         {
             trajectory = new Trajectory();
-            ticketClass = new TicketClass();
-            ticketDate = new TicketDate();
-            ticketType = new TicketType();
-            ticketDiscount = new TicketDiscount();
         }
     }
 }
